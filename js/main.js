dc6e44241829a4c4fa9722b6bf08b957764a357d
Pascaru Sergiu <pascarusergiu003@gmail.com>
2018-01-15 15:18:51 +0200

    js fix

--- js/main.js
+++ js/main.js
@@ -10,6 +10,6 @@ function carousel() {
     }
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].style.display = "block";
+    x[slideIndex-1].style.display = "flex";
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
